747455c87e12b778455bae67e6245ae54a44cdf9
rizkyawaluddin <rizkyawaluddin010498@gmail.com>
2022-08-27 00:31:02 +0700

    first commit

---
+++ server/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"waysbucks/database"
+	"waysbucks/pkg/mysql"
+	"waysbucks/routes"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	errEnv := godotenv.Load()
+		if errEnv != nil {
+      panic("Failed to load env file")
+    }
+
+	mysql.DatabaseInit()
+
+	database.RunMigration()
+
+	r := mux.NewRouter()
+
+	routes.RouteInit(r.PathPrefix("/api/v1").Subrouter())
+
+	// Setup allowed Header, Method, and Origin for CORS on this below code ...
+	var AllowedHeaders = handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	var AllowedMethods = handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS", "PATCH", "DELETE"})
+	var AllowedOrigins = handlers.AllowedOrigins([]string{"*"})
+
+	//path file
+	r.PathPrefix("/uploads").Handler(http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
+
+	//try
+	fmt.Println("Server Running on localhost:5000")
+	http.ListenAndServe("localhost:5000", handlers.CORS(AllowedHeaders, AllowedMethods, AllowedOrigins)(r))
+
+}
